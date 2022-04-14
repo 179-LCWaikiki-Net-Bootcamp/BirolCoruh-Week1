416ea5af0b37ba5d07d467e4c2f69fb1ab77ff9f
Birol Coruh <65693604+birolcoruh26@users.noreply.github.com>
2022-04-14 22:08:19 +0300

    Added

---
+++ StoreApi/StoreApi/Controllers/CategoryController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StoreApi.DbOperations;
+using StoreApi.Entities;
+
+namespace StoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly StoreDbContext _context;
+        public CategoryController(StoreDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetCategories()
+        {
+            var categories = _context.Categories.OrderBy(x => x.Id).ToList<Category>();
+            return Ok(categories);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCategoryById(int id)
+        {
+            var category = _context.Categories.Where(category => category.Id == id).SingleOrDefault();
+            if (category == null)
+            {
+                return BadRequest("Kategori Bulunamadı");
+            }
+            else
+            {
+                return Ok(category);
+            }
+        }
+        [HttpGet("{name}")]
+        public IActionResult GetCategoryByName(string name)
+        {
+            var category = _context.Categories.Where(category => category.Name == name).SingleOrDefault();
+            if (category == null)
+            {
+                return NotFound("Kategori Bulunamadı");
+            }
+            else
+            {
+                return Ok(category);
+            }
+        }
+        [HttpPost]
+        public IActionResult AddCategory([FromBody] Category newCategory)
+        {
+            var category = _context.Categories.SingleOrDefault(x => x.Id == newCategory.Id);
+
+            if (category != null)
+            {
+                return NotFound("Eklenecek Kategori Bulunamadı");
+            }
+            else
+            {
+                _context.Categories.Add(category);
+                _context.SaveChangesAsync();
+                return Created("Kategori Eklendi", category);
+            }
+        }
+        [HttpPut("{id}")]
+        public IActionResult UpdateCategory(int id, [FromBody] Category newCategory)
+        {
+            var category = _context.Categories.SingleOrDefault(x => x.Id == newCategory.Id);
+
+            if (category != null)
+            {
+                return NotFound("Eklenecek Kategori Bulunamadı");
+            }
+            else
+            {
+                _context.Categories.Add(category);
+                return Created("Kategori Eklendi", category);
+            }
+        }
+        [HttpDelete("{id}")]
+        public ActionResult DeleteCategory(int id)
+        {
+            var category = _context.Categories.SingleOrDefault(x => x.Id == id);
+
+            if (category == null)
+            {
+                return NotFound("Silinecek Kategori Bulunamadı");
+            }
+            else
+            {
+                _context.Categories.Remove(category);
+                _context.SaveChanges();
+                return Ok("Ürün Silindi!");
+            }
+
+        }
+    }
+}
---
+++ StoreApi/StoreApi/Controllers/ProductController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StoreApi.DbOperations;
+using StoreApi.Entities;
+
+namespace StoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly StoreDbContext _context;
+        public ProductController(StoreDbContext context)
+        {
+            this._context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            var products = _context.Products.OrderBy(x => x.Id).ToList<Product>();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetProductById(int id)
+        {
+            var product = _context.Products.Where(Product => Product.Id == id).SingleOrDefault();
+            if (product == null)
+            {
+                return BadRequest("Ürün Bulunamadı");
+            }
+            else
+            {
+                return Ok(product);
+            }
+        }
+        [HttpGet("{name}")]
+        public IActionResult GetProductByName(string name)
+        {
+            var product = _context.Products.Where(Product => Product.Name == name).SingleOrDefault();
+            if (product == null)
+            {
+                return NotFound("Ürün Bulunamadı");
+            }
+            else
+            {
+                return Ok(product);
+            }
+        }
+        [HttpPost]
+        public IActionResult AddProduct([FromBody] Product newProduct)
+        {
+            var product = _context.Products.SingleOrDefault(x => x.Id == newProduct.Id);
+
+            if (product != null)
+            {
+                return NotFound("Eklenecek Ürün Bulunamadı");
+            }
+            else
+            {
+                _context.Products.Add(product);
+                _context.SaveChangesAsync();
+                return Created("Ürün Eklendi", product);
+            }
+        }
+        [HttpPut("{id}")]
+        public IActionResult UpdateProduct(int id, [FromBody] Product newProduct)
+        {
+            var product = _context.Products.SingleOrDefault(x => x.Id == newProduct.Id);
+
+            if (product != null)
+            {
+                return NotFound("Eklenecek Ürün Bulunamadı");
+            }
+            else
+            {
+                _context.Products.Add(product);
+                return Created("Ürün Eklendi", product);
+            }
+        }
+        [HttpDelete("{id}")]
+        public ActionResult DeleteProduct(int id)
+        {
+            var product = _context.Products.SingleOrDefault(x => x.Id == id);
+
+            if (product == null)
+            {
+                return NotFound("Silinecek Ürün Bulunamadı");
+            }
+            else
+            {
+                _context.Products.Remove(product);
+                _context.SaveChanges();
+                return Ok("Ürün Silindi!");
+            }
+
+        }
+    }
+}
